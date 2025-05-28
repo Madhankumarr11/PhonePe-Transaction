e6fffb53578dca48f6deff23825c2bc4f5c0afd1
Madhankumarr11 <srmadhan95@gmail.com>
2025-05-29 00:14:55 +0530

    for loop

--- OS.py
+++ OS.py
@@ -1,4 +1,38 @@
 import os
-path = r'D:\SRMK\Guvi\PhonePe-Transaction\pulse\data\aggregated\user\country\india\state'
+import json
+# import pandas as pd
+
+#Once created the clone of GIT-HUB repository then,
+#Required libraries for the program
+
+#This is to direct the path to get the data as states
+
+path = 'D:/SRMK/Guvi/PhonePe-Transaction/pulse/data/aggregated/user/country/india/state/'
 Agg_state_list = os.listdir(path)
-Agg_state_list 
+
+#Agg_state_list--> to get the list of states in India
+
+#<------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------>#
+
+#This is to extract the data's to create a dataframe
+
+for state in Agg_state_list:
+    cur_states  = path + state + '/'
+    Agg_year_list = os.listdir(cur_states)
+
+    for year in Agg_year_list:
+        cur_years = cur_states + year + '/'
+        Agg_file_list = os.listdir(cur_years)
+
+        for file in Agg_file_list:
+            cur_files = cur_years + file
+            data = open (cur_files)
+            read = data.read()   # read issue clear code
+
+            print(read)
+            
+        
+
+
+
+
