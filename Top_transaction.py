3be08577fa63785ade0985764ec1f10d2c582587
Madhankumarr11 <srmadhan95@gmail.com>
2025-06-06 22:36:55 +0530

    DataFrame Add

---
+++ Top_transaction.py
@@ -0,0 +1,40 @@
+import os
+import json
+import pandas as pd
+
+path7 = 'D:/SRMK/Guvi/PhonePe-Transaction/pulse/data/top/transaction/country/india/state/'
+top_transaction_list = os.listdir(path7)
+
+column7 = {'State': [], 'Year': [], 'Quarter': [], 'Pincode': [], 'Transaction_count': [], 'Transaction_amount': []}
+
+for state in top_transaction_list:
+    cur_state = path7 + state + '/'
+    top_year_list = os.listdir(cur_state)
+
+    for year in top_year_list:
+        cur_year = cur_state + year + '/'
+        top_file_list = os.listdir(cur_year)
+
+        for file in top_file_list:
+            cur_file = cur_year + file
+            data = open(cur_file,'r')
+
+            G = json.load(data)
+
+            for i in G['data']['pincodes']:
+                entityname = i['entityName']
+                count = i['metric']['count']
+                amount = i['metric']['amount']
+                column7['Pincode'].append(entityname)
+                column7['Transaction_count'].append(count)
+                column7['Transaction_amount'].append(amount)
+                column7['State'].append(state)
+                column7['Year'].append(year)
+                column7['Quarter'].append(int(file.strip('.json')))
+
+#Succesfully created a dataframe
+Top_transaction = pd.DataFrame(column7)
+
+print(Top_transaction)
+
+
